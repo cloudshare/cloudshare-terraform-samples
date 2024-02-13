c35e4e9ef60e84eba791b7fbec20b03b1bd84b17
akoslik <alex.koslik@cloudshare.com>
2024-02-13 17:40:32 +0200

    update

--- AWS/aws-macos.tf
+++ AWS/aws-macos.tf
@@ -7,11 +7,11 @@ resource "aws_instance" "example_instance" {
   ami           = data.aws_ami.mac2-m2-ami.id
   host_id       = aws_ec2_host.example_host.id
   instance_type = "mac2-m2.metal"
+  iam_instance_profile = var.CS_AWS_SSM_Instance_Profile_Name
   key_name      = "cs-key"
   tags = {
     Name = "macOS"
   }
-  iam_instance_profile = var.CS_AWS_SSM_Instance_Profile_Name
 }
 
 data "aws_ami" "mac2-m2-ami" {
