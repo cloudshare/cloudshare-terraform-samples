5d4a81e1c68c7c97f09e522d4686de5c2d134c53
akoslik <alex.koslik@cloudshare.com>
2024-02-13 15:34:09 +0200

    Create aws-macos.tf

---
+++ AWS/aws-macos.tf
@@ -0,0 +1,24 @@
+resource "aws_ec2_host" "example_host" {
+  instance_type     = "mac2-m2.metal"
+  availability_zone = "eu-central-1a"
+}
+
+resource "aws_instance" "example_instance" {
+  ami           = data.aws_ami.mac2-m2-ami.id
+  host_id       = aws_ec2_host.example_host.id
+  instance_type = "mac2-m2.metal"
+  key_name      = "cs-key"
+  tags = {
+    Name = "macOS"
+  }
+  iam_instance_profile = var.CS_AWS_SSM_Instance_Profile_Name
+}
+
+data "aws_ami" "mac2-m2-ami" {
+  owners      = ["amazon"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn-ec2-macos-13.6.3*"]
+  }
+}
