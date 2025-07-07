a3849138d07349c25f709b6dc2934362324c733d
root <ofir@cloudshare.com>
2025-07-07 11:23:33 +0000

    Update Azure Linux examples

--- Azure/linux-public-image-with-custom-script.tf
+++ Azure/linux-public-image-with-custom-script.tf
@@ -6,47 +6,48 @@ variable "prefix" {
   default = "cloudshare"
 }
 
-resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = "${data.azurerm_resource_group.main.location}"
-  resource_group_name = "${data.azurerm_resource_group.main.name}"
-}
-
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = "${data.azurerm_resource_group.main.name}"
-  virtual_network_name = "${azurerm_virtual_network.main.name}"
-  address_prefix       = "10.0.2.0/24"
-}
-
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
   location            = "${data.azurerm_resource_group.main.location}"
   resource_group_name = "${data.azurerm_resource_group.main.name}"
 
   ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = "${azurerm_subnet.internal.id}"
+    name                          = "${var.prefix}-configuration"
+    subnet_id                     = "${azurerm_subnet.subnet.id}"
     private_ip_address_allocation = "Dynamic"
   }
 }
 
+resource "azurerm_virtual_network" "vnet" { 
+  name                = "${var.prefix}-network"  
+  address_space       = ["10.0.0.0/16"]  
+  location            = "${data.azurerm_resource_group.main.location}"  
+  resource_group_name = "${data.azurerm_resource_group.main.name}"  
+}  
+  
+resource "azurerm_subnet" "subnet" {   
+  name                 = "${var.prefix}-subnet"  
+  resource_group_name  = "${data.azurerm_resource_group.main.name}"  
+  virtual_network_name = "${azurerm_virtual_network.vnet.name}"  
+  address_prefixes       = ["10.0.2.0/24"] 
+}  
+  
 resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-ubuntu-16"
+  name                  = "${var.prefix}-ubuntu-24"
   location              = "${data.azurerm_resource_group.main.location}"
+
   resource_group_name   = "${data.azurerm_resource_group.main.name}"
   network_interface_ids = ["${azurerm_network_interface.main.id}"]
   vm_size               = "Standard_B1ls"
 
   storage_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
     version   = "latest"
   }
   storage_os_disk {
-    name              = "myosdisk1"
+    name              = "linuxosdisk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
@@ -58,9 +59,6 @@ resource "azurerm_virtual_machine" "main" {
   }
   os_profile_linux_config {
     disable_password_authentication = false
-  }
-  tags = {
-    environment = "staging"
   }
 }
 
@@ -78,8 +76,4 @@ resource "azurerm_virtual_machine_extension" "myterraformvm" {
     "commandToExecute": "/bin/bash <SCRIPT_NAME.SH>"
   }
   SETTINGS
-
-  tags = {
-    environment = "staging"
-  }
 }
